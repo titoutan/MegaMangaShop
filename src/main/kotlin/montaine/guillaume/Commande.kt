2137a67680594d03e58845f23b053cc20f90ebe3
Antoine Delagnes <antoine.delagnes@etu.iut-tlse3.fr>
2024-12-09 15:10:19 +0100

    feat: test <150 mangas

--- src/main/kotlin/montaine/guillaume/Commande.kt
+++ src/main/kotlin/montaine/guillaume/Commande.kt
@@ -3,7 +3,7 @@ package montaine.guillaume
 import de.huxhorn.sulky.ulid.ULID;
 
 
-data class LigneCommande(val Id: String = ULID().nextULID() ) {
+data class Commande(val Id: String = ULID().nextULID() ) {
 
     val Ligne: List<LigneCommande> = ArrayList<>()
 
--- src/test/kotlin/org/example/montaine/guillaume/tests/MontaineGuillaumeTest.kt
+++ src/test/kotlin/org/example/montaine/guillaume/tests/MontaineGuillaumeTest.kt
@@ -2,11 +2,18 @@
 package org.example.montaine.guillaume.tests
 
 import io.kotest.core.spec.style.BehaviorSpec
-
+import montaine.guillaume.LigneCommande
 class MontaineGuillaumeTest: BehaviorSpec({
 
     context("je veux vérifier les reductions par état et selon les montants") {
         given("<150 mangas") {
-            
+            val ligneCommande = LigneCommande()
+            ligneCommande.setQuantite(100)
+            ligneCommande.setPrix(5.0)
+            val commande= Commande()
+            commande.ajouterLigne(ligneCommande)
+            val tiquetDeCaisse= TiquetDeCaisse()
+            tiquetDeCaisse.calculer(commande)
+            tiquetDeCaisse.getPrix() shouldBe 500
         }
     }})
